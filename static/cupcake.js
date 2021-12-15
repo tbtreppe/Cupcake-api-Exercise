1c91869fe0238e457ba02653bbe45dccbe912a28
Tracey <ttrepper@yahoo.com>
2021-12-14 20:17:13 -0500

    fixed bugs

--- static/cupcake.js
+++ static/cupcake.js
@@ -1,7 +1,17 @@
 const BASE_URL = "http://localhost:5000/api";
 
 function getCupcakeHTML(cupcake) {
-  return `<div data-cucpake-id={cupcake.id}> <li> ${cupcake.flavor} / ${cupcake.size} / ${cupcake.rating} <button class="delete-button"></button></li> <img class="Cupcake-img" src="${cupcake.image}" alt="(no image provided)"></div>`;
+  return `
+    <div data-cupcake-id=${cupcake.id}>
+      <li>
+        ${cupcake.flavor} / ${cupcake.size} / ${cupcake.rating}
+        <button class="delete-button">Delete</button>
+      </li>
+      <img class="Cupcake-img"
+            src="${cupcake.image}"
+            alt="(no image provided)">
+    </div>
+  `;
 }
 
 async function showCupcakes() {
@@ -13,13 +23,7 @@ async function showCupcakes() {
   }
 }
 
-$(".add-cupcake").click(addCupcake);
-async function addCupcake() {
-  await axios.post(`/api/cupcake`);
-  $(this).parent().append();
-}
-
-$("new-cupcake-form").on("submit", async function (evt) {
+$("#new-cupcake-form").on("submit", async function (evt) {
   evt.preventDefault();
 
   let flavor = $("#form-flavor").val();
@@ -34,14 +38,14 @@ $("new-cupcake-form").on("submit", async function (evt) {
     image,
   });
   let newCupcake = $(getCupcakeHTML(newCupcakeResponse.data.cupcake));
-  $("#cupcakes-list").append(newCupcake);
+  $("#cupcake-list").append(newCupcake);
   $("#new-cupcake-form").trigger("reset");
 });
 
 $("#cupcake-list").on("click", ".delete-button", async function (evt) {
   evt.preventDefault();
   let $cupcake = $(evt.target).closest("div");
-  let cupcakeId = $cupcake.attr("data-cupcake-ide");
+  let cupcakeId = $cupcake.attr("data-cupcake-id");
 
   await axios.delete(`${BASE_URL}/cupcakes/${cupcakeId}`);
   $cupcake.remove();
